b6f8b4bf64a30493e38295b1f560fec2b05b9b86
Brian Samek <brian.samek@gmail.com>
2015-11-05 22:46:42 -0500

    Comment and refactor.

--- init.el
+++ init.el
@@ -1,9 +1,11 @@
-(package-initialize)
+;; Set up MELPA
+(require 'package)
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(when (not package-archive-contents) (package-refresh-contents))
+(unless package-archive-contents (package-refresh-contents))
+(package-initialize)
 
-(unless (package-installed-p 'use-package)
-  (message "`use-package' not found. Installing...")
-  (package-install 'use-package))
+;; Install use-package if it's not present
+(unless (package-installed-p 'use-package) (package-install 'use-package))
 (require 'use-package)
+
