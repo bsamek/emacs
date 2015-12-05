e18c62b8112cf05057bc0498743b0315fd07b687
Brian Samek <brian.samek@gmail.com>
2015-12-05 00:35:42 -0500

    Added additional commands to golden-ratio.
    Golden ratio was only working with C-x o, not with under forms of
    window switching.

--- init.el
+++ init.el
@@ -78,7 +78,23 @@
 (use-package go-mode)
 
 (use-package golden-ratio
-  :config (golden-ratio-mode 1)
+  :config
+  (golden-ratio-mode 1)
+  (setq golden-ratio-extra-commands
+	(append golden-ratio-extra-commands
+		'(evil-window-left
+		  evil-window-right
+		  evil-window-up
+		  evil-window-down
+		  select-window-1
+		  select-window-2
+		  select-window-3
+		  select-window-4
+		  select-window-5
+		  select-window-6
+		  select-window-7
+		  select-window-8
+		  select-window-9)))
   :diminish golden-ratio-mode)
 
 (use-package guide-key
