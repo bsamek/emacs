16c7c1c75562a6e33df33995773a8981337af1ea
Brian Samek <bsamek@users.noreply.github.com>
2026-06-30 22:42:36 -0400

    Move Emacs Custom state out of init

--- init.el
+++ init.el
@@ -3,6 +3,11 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; Keep Customize-generated settings out of this version-controlled init file.
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 ;; Keep GNU ELPA signing keys current before packages install dependencies.
 (unless (package-installed-p 'gnu-elpa-keyring-update)
   (unless package-archive-contents
@@ -294,19 +299,3 @@
   :config
   (when (executable-find "xclip")
     (xclip-mode 1)))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("aec7b55f2a13307a55517fdf08438863d694550565dee23181d2ebd973ebd6b8"
-     default))
- '(package-selected-packages nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
